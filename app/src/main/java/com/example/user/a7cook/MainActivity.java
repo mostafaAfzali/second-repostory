434a9f6a190fa7ce19414620b6f241b13f07c86e
mostafa <mafzali99@yahoo.com>
2018-08-31 13:58:24 +0430

    some changes

--- app/src/main/java/com/example/user/a7cook/MainActivity.java
+++ app/src/main/java/com/example/user/a7cook/MainActivity.java
@@ -80,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
                 SnapHelper snapHelper = new PagerSnapHelper();
                 snapHelper.attachToRecyclerView(bannerRv);
+                //some changes
             }
         });
     }
